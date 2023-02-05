45419a433defd63c7bf09f8ef60c8658f0026f28
“Ares” <AresDev@proton.me>
2023-02-05 16:17:16 +0100

    Finished simple implementation of a buy order

--- src/contracts/Orderbook.sol
+++ src/contracts/Orderbook.sol
@@ -70,6 +70,17 @@ contract OrderBook{
 
     }
 
+    function removeSellOrder(uint index) internal{
+        if (index >= sellOrders.length) return;
+
+        for (uint i = index; i<sellOrders.length-1; i++){
+            sellOrders[i] = sellOrders[i+1];
+        }
+        delete sellOrders[sellOrders.length-1];
+        sellOrders.pop();
+        
+    }
+
     function removeBuyOrder(uint index) internal{
         if (index >= buyOrders.length) return;
 
@@ -82,37 +93,66 @@ contract OrderBook{
     }
 
     function checkAndFullfillBuys() internal{
-        BuyOrder memory current = buyOrders[0];
+        BuyOrder memory current = buyOrders[buyOrders.length-1];
         //uint startingAmount = current.amount;
         uint i = sellOrders.length;
-        while(i>=0 && buyOrders[0].amount!=0){
-            if(sellOrders[i].minPrice >= current.maxPrice){
+        while(i>=0 && buyOrders[buyOrders.length-1].amount!=0){
+            if(sellOrders[i].minPrice <= current.maxPrice){
                 if(sellOrders[i].amount <= current.amount){
-                    buyOrders[0].amount -= sellOrders[i].amount;
-                    current = buyOrders[0];
+                    buyOrders[buyOrders.length-1].amount -= sellOrders[i].amount;
+                    current = buyOrders[buyOrders.length-1];
                     fullfilledSell.push(sellOrders[i]);
                     delete sellOrders[i];
+                    sellOrders.pop();
                 }else{
                     SellOrder memory tempSell = SellOrder(sellOrders[i].seller, sellOrders[i].minPrice, current.amount);
                     sellOrders[i].amount -= current.amount;
-                    buyOrders[0].amount = 0;
-                    current = buyOrders[0];
+                    buyOrders[buyOrders.length-1].amount = 0;
+                    current = buyOrders[buyOrders.length-1];
                     fullfilledSell.push(tempSell);
                     delete tempSell;
                 }
             }
+            i--;
 
         }
-        if(buyOrders[0].amount==0){
-            removeBuyOrder(0);
+        if(buyOrders[buyOrders.length-1].amount==0){
+            removeBuyOrder(buyOrders.length-1);
         }
+    }
+
+
+    function calculatePriceDifferenceAndAmount(uint256 maxCost) view internal returns(uint256 priceDifference, uint256 amount){
+        uint priceDif = 0;
+        uint totalAmount = 0;
+        for(uint i = 0; i<fullfilledSell.length;i++){
+            priceDif += fullfilledSell[i].amount * (maxCost-fullfilledSell[i].minPrice);
+            totalAmount += fullfilledSell[i].amount;
+        }
+
+        return(priceDif, totalAmount);
+        
+    }
+
+    function payOut(uint256 priceDif, uint256 amount) internal{
+        
+        //payout priceDif to buyer
+
+        // payout DAI to sellers and Bitcoin to buyer
+
+        // DAI from fullFilledSell
+        // BTC amount from amount
     }
 
     function addBuyOrder(address _buyer, uint256 _maxPrice, uint256 _amount) external onlyBridge {
         uint index = getBuyIndex(_maxPrice);
         createNewBuyArray(_buyer, _maxPrice, _amount, index);
-        if(index==0){
+        if(index==buyOrders.length-1){
             checkAndFullfillBuys();
+            (uint256 priceDifference, uint256 amount) = calculatePriceDifferenceAndAmount(_maxPrice);
+            payOut(priceDifference, amount);
+            // We could implement recursion here in case we want to increase the Max Price a buyer is willing to pay if he got the previous Bitcoins cheaper than his maxPrice
+            delete fullfilledSell;
         }
 
     }
@@ -124,101 +164,3 @@ contract OrderBook{
 
 
 
-
-
-/// @notice Purpose struct
-struct Purpose {
-    address owner;
-    string purpose;
-    uint256 investment;
-}
-
-/**
- @title A contract to set a World Purpose
- @author Emanuele Ricci @StErMi
-*/
-contract WorldPurpose {
-    /// @notice Track user investments
-    mapping(address => uint256) private balances;
-
-    /// @notice Track the current world purpose
-    Purpose private purpose;
-
-    /// @notice Event to track new Purpose
-    event PurposeChange(address indexed owner, string purpose, uint256 investment);
-
-    /**
-     @notice Getter for the current purpose
-     @return currentPurpose The current active World purpose
-    */
-    function getCurrentPurpose() public view returns (Purpose memory currentPurpose) {
-        return purpose;
-    }
-
-    /**
-     @notice Get the total amount of investment you have made. It returns both the locked and unloacked investment.
-     @return balance The balance you still have in the contract
-    */
-    function getBalance() public view returns (uint256 balance) {
-        return balances[msg.sender];
-    }
-
-    /**
-     @notice Modifier to check that the prev owner of the purpose is not the same as the new proposer
-    */
-    modifier onlyNewUser() {
-        // Check that new owner is not the previous one
-        require(purpose.owner != msg.sender, "You cannot override your own purpose");
-        _;
-    }
-
-    /**
-     @notice Set the new world purpose
-     @param _purpose The new purpose content
-     @return newPurpose The new active World purpose
-    */
-    function setPurpose(string memory _purpose) public payable onlyNewUser returns (Purpose memory newPurpose) {
-        // Check that the new owner has sent us enough funds to override the previous purpose
-        require(msg.value > purpose.investment, "You need to invest more than the previous purpose owner");
-
-        // Check if the new purpose is empty
-        bytes memory purposeBytes = bytes(_purpose);
-        require(purposeBytes.length > 0, "You need to set a purpose message");
-
-        // Update the purpose with the new one
-        purpose = Purpose(msg.sender, _purpose, msg.value);
-
-        // Update the sender value
-        balances[msg.sender] += msg.value;
-
-        // Emit the PurposeChange event
-        emit PurposeChange(msg.sender, _purpose, msg.value);
-
-        // Return the new purpose
-        return purpose;
-    }
-
-    /**
-     @notice Withdraw the funds from the old purpose. If you have an active purpose those funds are "locked"
-    */
-    function withdraw() public {
-        // Get the user's balance
-        uint256 withdrawable = balances[msg.sender];
-
-        // Now we need to check how much the user can withdraw
-        address currentOwner = purpose.owner;
-        if (currentOwner == msg.sender) {
-            withdrawable -= purpose.investment;
-        }
-
-        // Check that the user has enough balance to withdraw
-        require(withdrawable > 0, "You don't have enough withdrawable balance");
-
-        // Update the balance
-        balances[msg.sender] -= withdrawable;
-
-        // Transfer the balance
-        (bool sent, ) = msg.sender.call{value: withdrawable}("");
-        require(sent, "Failed to send user balance back to the user");
-    }
-}
