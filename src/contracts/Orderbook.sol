8d42b18f0a41c50ec9c71880d1382c6c33cb7d53
“Ares” <AresDev@proton.me>
2023-02-06 15:41:28 +0100

    Started implementing sell orders

--- src/contracts/Orderbook.sol
+++ src/contracts/Orderbook.sol
@@ -48,12 +48,22 @@ contract OrderBook{
         uint i = 0;
         while(i<buyOrders.length){
             if(price<=buyOrders[i].maxPrice){
-                index = i;
-                return(index);
+                return(i);
             }
             i++;
         }
-        return i+1;
+        return(i+1);
+    }
+
+    function getSellIndex(uint256 price) internal view returns(uint256 index){
+        uint i = 0;
+        while(i<sellOrders.length){
+            if(price>=sellOrders[i].minPrice){
+                return(i);
+            }
+            i++;
+        }
+        return(i+1);
     }
 
     function createNewBuyArray(address _buyer, uint256 _maxPrice, uint256 _amount, uint256 index) internal{
@@ -67,6 +77,20 @@ contract OrderBook{
 
         buyOrders = _buyOrders;
         delete _buyOrders;
+
+    }
+
+    function createNewSellArray(address _seller, uint256 _minPrice, uint256 _amount, uint256 index) internal{
+        for(uint i=0;i<index;i++){
+            _sellOrders.push(sellOrders[i]);
+        }
+        _sellOrders.push(SellOrder(_seller,_minPrice,_amount));
+        for(uint i=index;i<sellOrders.length;i++){
+            _sellOrders.push(sellOrders[i]);
+        }
+
+        sellOrders = _sellOrders;
+        delete _sellOrders;
 
     }
 
@@ -92,10 +116,21 @@ contract OrderBook{
         
     }
 
+    function removeSellOrder(uint index) internal{
+        if (index >= sellOrders.length) return;
+
+        for (uint i = index; i<sellOrders.length-1; i++){
+            sellOrders[i] = sellOrders[i+1];
+        }
+        delete sellOrders[buyOrders.length-1];
+        sellOrders.pop();
+        
+    }
+
     function checkAndFullfillBuys() internal{
         BuyOrder memory current = buyOrders[buyOrders.length-1];
         //uint startingAmount = current.amount;
-        uint i = sellOrders.length;
+        uint i = sellOrders.length-1;
         while(i>=0 && buyOrders[buyOrders.length-1].amount!=0){
             if(sellOrders[i].minPrice <= current.maxPrice){
                 if(sellOrders[i].amount <= current.amount){
@@ -118,6 +153,36 @@ contract OrderBook{
         }
         if(buyOrders[buyOrders.length-1].amount==0){
             removeBuyOrder(buyOrders.length-1);
+        }
+    }
+
+
+    function checkAndFullfillSell() internal{
+        SellOrder memory current = sellOrders[sellOrders.length-1];
+        //uint startingAmount = current.amount;
+        uint i = buyOrders.length-1;
+        while(i>=0 && sellOrders[sellOrders.length-1].amount!=0){
+            if(buyOrders[i].maxPrice >= current.minPrice){
+                if(buyOrders[i].amount <= current.amount){
+                    sellOrders[sellOrders.length-1].amount -= buyOrders[i].amount;
+                    current = sellOrders[sellOrders.length-1];
+                    fullfilledBuy.push(buyOrders[i]);
+                    delete buyOrders[i];
+                    buyOrders.pop();
+                }else{
+                    BuyOrder memory tempBuy = BuyOrder(buyOrders[i].buyer, buyOrders[i].maxPrice, current.amount);
+                    buyOrders[i].amount -= current.amount;
+                    sellOrders[sellOrders.length-1].amount = 0;
+                    current = sellOrders[sellOrders.length-1];
+                    fullfilledBuy.push(tempBuy);
+                    delete tempBuy;
+                }
+            }
+            i--;
+
+        }
+        if(sellOrders[sellOrders.length-1].amount==0){
+            removeSellOrder(sellOrders.length-1);
         }
     }
 
@@ -150,6 +215,19 @@ contract OrderBook{
         if(index==buyOrders.length-1){
             checkAndFullfillBuys();
             (uint256 priceDifference, uint256 amount) = calculatePriceDifferenceAndAmount(_maxPrice);
+            payOut(priceDifference, amount);
+            // We could implement recursion here in case we want to increase the Max Price a buyer is willing to pay if he got the previous Bitcoins cheaper than his maxPrice
+            delete fullfilledSell;
+        }
+
+    }
+
+    function addSellOrder(address _seller, uint256 _minPrice, uint256 _amount) external onlyBridge {
+        uint index = getSellIndex(_minPrice);
+        createNewSellArray(_seller, _minPrice, _amount, index);
+        if(index==sellOrders.length-1){
+            checkAndFullfillSell();
+            (uint256 priceDifference, uint256 amount) = calculatePriceDifferenceAndAmount(_minPrice);
             payOut(priceDifference, amount);
             // We could implement recursion here in case we want to increase the Max Price a buyer is willing to pay if he got the previous Bitcoins cheaper than his maxPrice
             delete fullfilledSell;
